03e546012bab2bcfe0d4227dc8ebca262aeff0fe
Wiktor Kozłowski <kozlowskiwiktor92@gmail.com>
2024-02-11 21:23:34 +0100

    added ContactList

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,25 @@
+import propTypes from 'prop-types';
+
+const ContactList = ({ contacts, onDeleteContact }) => {
+  return (
+    <div>
+      {contacts.map(contact => {
+        return (
+          <p key={contact.id}>
+            {contact.name} {contact.number}
+            <button type="submit" onClick={() => onDeleteContact(contact.id)}>
+              delete
+            </button>
+          </p>
+        );
+      })}
+    </div>
+  );
+};
+
+export default ContactList;
+
+ContactList.propTypes = {
+  contacts: propTypes.arrayOf(propTypes.object),
+  onDeleteContact: propTypes.func,
+};
