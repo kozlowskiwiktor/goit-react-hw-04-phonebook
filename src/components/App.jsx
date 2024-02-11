3cb711677399b6455a873986e35a1a666a7364f2
Wiktor Kozłowski <kozlowskiwiktor92@gmail.com>
2024-02-11 21:23:54 +0100

    Updated App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,69 @@
+import { useState, useEffect } from 'react';
+
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+
 export const App = () => {
+  const [filter, setFilter] = useState('');
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    const savedContacts = localStorage.getItem('contacts');
+
+    if (savedContacts) {
+      const parsedContacts = JSON.parse(savedContacts);
+      setContacts(parsedContacts);
+    }
+  }, []);
+
+  const addContactToPhoneBook = newContact => {
+    const isExists = contacts.find(
+      contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
+    );
+    if (isExists) {
+      return alert(`${isExists.name} is already in contacts.`);
+    }
+    const updatedContacts = [...contacts, newContact];
+
+    setContacts(updatedContacts);
+
+    localStorage.setItem('contacts', JSON.stringify(updatedContacts));
+  };
+
+  const getContacts = (contacts, filter) => {
+    const normalizeName = filter.toLowerCase();
+
+    console.log(normalizeName);
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizeName)
+    );
+  };
+
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value);
+  };
+
+  const deleteContact = id => {
+    const updatedContacts = contacts.filter(contact => contact.id !== id);
+    setContacts(updatedContacts);
+
+    localStorage.setItem('contacts', JSON.stringify(updatedContacts));
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <ContactForm onSubmit={addContactToPhoneBook} />
+      <Filter value={filter} onChangeFilter={changeFilter} />
+      {contacts.length ? (
+        <ContactList
+          contacts={getContacts(contacts, filter)}
+          onDeleteContact={deleteContact}
+        />
+      ) : (
+        <p>No contact</p>
+      )}
     </div>
   );
 };
